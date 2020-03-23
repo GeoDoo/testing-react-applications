dc10e1eb80a9d1bbbe7c5857948d3ebd719bb1de
Georgios Karametas <geometas@hotmail.com>
2020-03-23 13:08:23 +0000

    Basic app webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    host: "localhost"
+  },
+  mode: "development"
+};
