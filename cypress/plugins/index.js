6fc4e0a6f12003d6a26f1e3ba9a7675498c09307
George Karametas <georgios.karametas@viooh.com>
2020-04-30 21:08:35 +0100

    Setup Cypress with Cucumber

---
+++ cypress/plugins/index.js
@@ -0,0 +1,11 @@
+const webpack = require('@cypress/webpack-preprocessor')
+const webpackOptions = require('../../webpack.config.js')
+
+module.exports = (on, _config) => {
+  on(
+      'file:preprocessor',
+      webpack({
+        webpackOptions,
+      }),
+  )
+}
