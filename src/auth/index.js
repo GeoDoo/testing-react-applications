5942e5a2d0a6ec223d1a6cfa34c24385198521a7
George Karametas <georgios.karametas@viooh.com>
2020-05-01 21:42:00 +0100

    Base64 mock for token

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,7 +2,7 @@ import { user } from './../__test-data__/db.json'
 
 const getToken = async (username, password) => {
   if (username === user.email && password === user.password) {
-    return 'aljdasdIAD(£($*ASHFASDJKSDIASD*@£HN@£JACIAIDAHSDAJDJASD'
+    return btoa(JSON.stringify({ email: user.email }))
   }
 
   return ''
