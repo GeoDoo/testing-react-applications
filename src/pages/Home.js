edbbadfc3552c4c211048a08bb73a7464c4e9d9d
George Karametas <georgios.karametas@viooh.com>
2020-05-02 20:00:32 +0100

    Add Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ThreePartsLayout from '../layouts/3_parts'
 
-const Home = () => (
-  <ThreePartsLayout></ThreePartsLayout>
-)
+const Home = () => <ThreePartsLayout></ThreePartsLayout>
 
 export default Home
